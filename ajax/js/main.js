682078e6ba87494e18e33815cc06b4a97c9b650c
Adam Czyzewski <aczyze@gmail.com>
2019-05-12 10:00:18 +0200

    ajax start

---
+++ ajax/js/main.js
@@ -0,0 +1,10 @@
+// AJAX FETCH
+// Pobierz dane z adresu URL - funkcja fetch(zwraca obiekt Promise - Obietnica)
+// Jeśli Promise zostanie rozwiązany to fetch() zwróci obiekt odpowiedzi (response)
+// Zwracamy obiekt odpowiedzi przetworzony na obiekt JSON
+// Od tego momemtu możemy korzystać z tych danych
+// fetch('https://jsonplaceholder.typicode.com/posts') // lub /1 , co to oznaczalo?
+// .then(response =>response.json())
+// .then(response=>{
+// console.log(response);
+// });
