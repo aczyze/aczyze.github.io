6917be171c35274372ff9eee8a08fb9fa3390dec
Adam Czyzewski <aczyze@gmail.com>
2019-05-19 13:04:24 +0200

    scss do przerwy lunchowej

--- moj-projekt/gulpfile.js
+++ moj-projekt/gulpfile.js
@@ -1,2 +1,13 @@
-$('.carousel').carousel();
-// zadzialaloby bez tego
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('sass', () => {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
+});
+
